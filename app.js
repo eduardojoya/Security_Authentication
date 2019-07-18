7e0c5e41c825386e4c11083d41c706ad8771ebf9
eduardojoya <ebjoya@gmail.com>
2019-07-17 23:27:06 -0700

    Level 2 Encryption added mongoose encryption

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-//jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
 const app = express();
 
 app.use(express.static("public"));
@@ -17,7 +18,13 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String,
+    password: String
+});
+
+const secret = "Thisisbetweenyouandme.";
+userSchema.plugin(encrypt, {
+    secret: secret,
+    encryptedFields: ["password"]
 });
 
 const User = new mongoose.model("User", userSchema);
